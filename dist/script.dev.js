356f017961a4c68fa43c465e6c70080743df0714
carefullycarefree <hannah.franky.may@gmail.com>
2021-10-15 21:01:02 -0500

    build app

---
+++ dist/script.dev.js
@@ -0,0 +1,86 @@
+"use strict";
+
+// Goal: When a user enters a URL in the input and presses "Shorten it", the code will send a get request to shrtcode in order to receive back the shortened url. Once the url has been received, a new result will appear below the input box with the original url, the shortened url, and a button that allows the user to copy the shortened url. Old results will stack below new ones until the screen is refreshed.
+// Define elements
+var shortenButton = document.getElementById("shorten-button");
+var urlInputField = document.getElementById("url-input-field");
+var resultsList = document.getElementById("results");
+var urlToCopy = document.getElementsByClassName("newUrl");
+var copyButton = document.getElementsByClassName("copy-button");
+var errorMessageMobile = document.getElementById("error-mobile");
+var errorMessageDesktop = document.getElementById("error-desktop");
+var newUrlToShorten = "";
+var newlyShortenedUrl = "";
+resultsList.addEventListener("click", function (e) {
+  if (e.target.className == 'copy-button') {
+    var _urlToCopy = e.target.previousElementSibling;
+    copy(_urlToCopy, e.target);
+  }
+}); // Send a GET request to the API, waiting for the response
+
+function fetchShortenedUrl(url) {
+  var response, data, newestResultElement;
+  return regeneratorRuntime.async(function fetchShortenedUrl$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(fetch("https://api.shrtco.de/v2/shorten?url=".concat(url)));
+
+        case 3:
+          response = _context.sent;
+          _context.next = 6;
+          return regeneratorRuntime.awrap(response.json());
+
+        case 6:
+          data = _context.sent;
+          // Receive the response and save it
+          newlyShortenedUrl = data.result.short_link; // Display response on UI
+
+          newestResultElement = document.createElement("div");
+          newestResultElement.className = "result";
+          newestResultElement.innerHTML = "<p class=\"url\">".concat(newUrlToShorten, "</p>\n            <div class=\"expand\"></div>\n            <p class=\"newUrl\">").concat(newlyShortenedUrl, "</p>\n            <button data-url=\"").concat(newlyShortenedUrl, "\" class=\"copy-button\">Copy</button>");
+          resultsList.insertBefore(newestResultElement, resultsList.firstChild);
+          _context.next = 17;
+          break;
+
+        case 14:
+          _context.prev = 14;
+          _context.t0 = _context["catch"](0);
+          // Handle errors
+          console.log(_context.t0);
+
+        case 17:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0, 14]]);
+} // Listen to button press 
+
+
+shortenButton.addEventListener("click", function (event) {
+  if (urlInputField.value != "") {
+    // Remove errors if applicable
+    urlInputField.classList.remove("empty");
+    errorMessageMobile.classList.remove("visible");
+    errorMessageDesktop.classList.remove("visible"); // Get value from input field
+
+    newUrlToShorten = urlInputField.value; // Clear field
+
+    urlInputField.value = ""; // Make API call and create new html element with result
+
+    fetchShortenedUrl(newUrlToShorten);
+  } else {
+    errorMessageMobile.classList.add("visible");
+    urlInputField.classList.add("empty");
+    errorMessageDesktop.classList.add("visible");
+  }
+}); // Listen to copy button
+
+function copy(copyText, button) {
+  navigator.clipboard.writeText(copyText.innerHTML);
+  button.innerHTML = "Copied!";
+  button.classList.add("copied");
+}
---
+++ script.js
@@ -0,0 +1,73 @@
+// Goal: When a user enters a URL in the input and presses "Shorten it", the code will send a get request to shrtcode in order to receive back the shortened url. Once the url has been received, a new result will appear below the input box with the original url, the shortened url, and a button that allows the user to copy the shortened url. Old results will stack below new ones until the screen is refreshed.
+
+// Define elements
+const shortenButton = document.getElementById("shorten-button");
+const urlInputField = document.getElementById("url-input-field");
+const resultsList = document.getElementById("results");
+const urlToCopy = document.getElementsByClassName("newUrl");
+const copyButton = document.getElementsByClassName("copy-button");
+const errorMessageMobile = document.getElementById("error-mobile");
+const errorMessageDesktop = document.getElementById("error-desktop");
+
+let newUrlToShorten = "";
+let newlyShortenedUrl = "";
+
+resultsList.addEventListener("click", (e) => {
+    if (e.target.className == 'copy-button') {
+        let urlToCopy = e.target.previousElementSibling
+        copy(urlToCopy, e.target)
+    }
+})
+
+// Send a GET request to the API, waiting for the response
+async function fetchShortenedUrl(url) {
+    try {
+        let response = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+        let data = await response.json()
+        // Receive the response and save it
+        newlyShortenedUrl = data.result.short_link;
+        // Display response on UI
+        let newestResultElement = document.createElement("div");
+        newestResultElement.className = "result";
+        newestResultElement.innerHTML = 
+            `<p class="url">${newUrlToShorten}</p>
+            <div class="expand"></div>
+            <p class="newUrl">${newlyShortenedUrl}</p>
+            <button data-url="${newlyShortenedUrl}" class="copy-button">Copy</button>`;
+        resultsList.insertBefore(newestResultElement, resultsList.firstChild)
+    } catch(err) {
+        // Handle errors
+        console.log(err)
+    }
+}
+
+// Listen to button press 
+shortenButton.addEventListener("click", event => {
+    if (urlInputField.value != "") {
+        // Remove errors if applicable
+        urlInputField.classList.remove("empty")
+        errorMessageMobile.classList.remove("visible")
+        errorMessageDesktop.classList.remove("visible")
+        // Get value from input field
+        newUrlToShorten = urlInputField.value;
+        // Clear field
+        urlInputField.value = ""
+        // Make API call and create new html element with result
+        fetchShortenedUrl(newUrlToShorten)
+    } else {
+        errorMessageMobile.classList.add("visible")
+        urlInputField.classList.add("empty")
+        errorMessageDesktop.classList.add("visible")
+    }
+})
+
+// Listen to copy button
+function copy(copyText, button) {
+    navigator.clipboard.writeText(copyText.innerHTML)
+    button.innerHTML = "Copied!"
+    button.classList.add("copied")
+}
+
+
+
+
